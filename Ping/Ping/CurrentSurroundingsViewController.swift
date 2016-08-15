820644722e12cea83b2e7b65ad0ab651f7ffa89b
Thomas Minshull <thomasminshull@gmail.com>
2016-08-15 14:31:31 -0700

    created view for usersTableViewcell and started to wire up surroundings vc

--- Ping/Ping/CurrentSurroundingsViewController.swift
+++ Ping/Ping/CurrentSurroundingsViewController.swift
@@ -11,67 +11,96 @@ import UIKit
 class CurrentSurroundingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     // Hard coded car data, replace with real people around
-    var tableData = ["Read 3 car articles", "Cleanup car", "Go for a ride", "Hit the road", "Build another car project", "Driving training", "Fix the layout problem of a client car project", "Get gas for car", "Check car parts dispatch status", "Booking the ticket to New York International Car Show", "Test the BMW M Performane Electronic Steering Wheel", "Call auto inspector"]
-    let themeBackGroundColor = UIColor(netHex:0xD9FAAA)
+//    var tableData = ["Read 3 car articles", "Cleanup car", "Go for a ride", "Hit the road", "Build another car project", "Driving training", "Fix the layout problem of a client car project", "Get gas for car", "Check car parts dispatch status", "Booking the ticket to New York International Car Show", "Test the BMW M Performane Electronic Steering Wheel", "Call auto inspector"]
+    
+    // MARK: Outlets
     @IBOutlet weak var longBackButton: UIButton!
     @IBOutlet weak var currentSurroundingTableView: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    var users = [User]()
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        currentSurroundingTableView.backgroundColor = themeBackGroundColor
+        currentSurroundingTableView.backgroundColor = UIColor(netHex:0xD9FAAA)
         currentSurroundingTableView.separatorStyle = UITableViewCellSeparatorStyle.None
         currentSurroundingTableView.tableFooterView = UIView(frame: CGRectZero)
-        currentSurroundingTableView.registerClass(SurroundingsTableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         
+        if users.count == 0 {
+            activityIndicator.startAnimating()
+            
+        }
     }
     
-    @IBAction func backButtonPressed(sender: AnyObject) {
-        self.dismissViewControllerAnimated(true, completion: nil)
-    }
-
-    
-    // MARK: - Navigation
-
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        
-    }
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
+    // MARK: TableView Delegate/DataSource
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return users.count
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 100
+        return 136
+        
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("cell");
+        let userCell = tableView.dequeueReusableCellWithIdentifier("UserCell", forIndexPath: indexPath) as! UserTableViewCell
         
-        cell!.textLabel?.text = tableData[indexPath.row]
-        cell!.textLabel?.textColor = UIColor.blackColor()
-        cell!.textLabel?.backgroundColor = UIColor.clearColor()
-        cell!.textLabel?.font = UIFont(name: "Helvetica Neue Thin", size: 22)
-        cell?.selectionStyle = UITableViewCellSelectionStyle.Default
+        userCell.selectionStyle = UITableViewCellSelectionStyle.Default
         
-        return cell!
-    }
-    
-    func colorForIndex(index: Int) -> UIColor {
+        userCell.configureWithUser(users[indexPath.row]);
         
-        let itemCount = tableData.count - 1
-        let transparency = (CGFloat(index) / CGFloat(itemCount)) * 0.6
-        
-        return UIColor(red: 0.85098, green: 0.98039, blue: 0.66667, alpha: transparency)
+        return userCell;
     }
     
     func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
         cell.backgroundColor =  colorForIndex(indexPath.row)
     }
+    
+    // MARK: Custom Methods
+    
+    func updateUsers() {
+        let recMan = RecordManager()
+        let sortedUUIDs = recMan.sortingUserRecordsInTimePeriodByProximity(NSDate()) ?? []
+        
+        users.removeAll()
+        let userMan = UserManager()
+        
+        for uuid:String in sortedUUIDs {
+            let user = userMan.userForUUID(uuid)
+            users.append(user)
+        }
+        
+    }
+    
+    func colorForIndex(index: Int) -> UIColor {
+        
+        let itemIndex = users.count - 1
+        let transparency = (CGFloat(index) / CGFloat(itemIndex)) * 0.6
+        
+        return UIColor(red: 0.85098, green: 0.98039, blue: 0.66667, alpha: transparency)
+    }
+    
+    
+    // MARK: Actions
+    
+    @IBAction func backButtonPressed(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    // MARK: Navigation
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+    }
+
 }
 
 
